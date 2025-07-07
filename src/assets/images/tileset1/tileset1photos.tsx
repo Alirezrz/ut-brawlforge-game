50da9706fa274a5a1ff4cad40114ccbb4260a45a
Alirezrz <alirezatu24@gmail.com>
2025-07-07 20:25:03 +0330

    added level1

---
+++ src/assets/images/tileset1/tileset1photos.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tileset1" tilewidth="256" tileheight="256" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="platform_left.png" width="256" height="256"/>
+ </tile>
+ <tile id="1">
+  <image source="platform_middle.png" width="256" height="256"/>
+ </tile>
+ <tile id="2">
+  <image source="platform_right.png" width="256" height="256"/>
+ </tile>
+ <tile id="3">
+  <image source="platform_solid.png" width="256" height="256"/>
+ </tile>
+</tileset>
