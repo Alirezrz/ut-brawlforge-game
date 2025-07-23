0abd026fc6d63f7905d33f57d8733f440e44aa21
Alirezrz <alirezatu24@gmail.com>
2025-07-23 21:44:43 +0330

    bug fix for gunamn

--- src/engine/gunman.py
+++ src/engine/gunman.py
@@ -103,17 +103,20 @@ class Gunman:
 
     def display(self, screen, offset):
         self.display_health_bar(screen,offset)
-
-        if self.Look == 'right':
-            screen.blit(self.display_frame, (self.x_pos - offset[0], self.y_pos - offset[1]))
-        else:
-            screen.blit(pygame.transform.flip(self.display_frame, True, False), (self.x_pos - offset[0], self.y_pos - offset[1]))
+        if not self.death_finished:
+            if self.Look == 'right':
+                screen.blit(self.display_frame, (self.x_pos - offset[0], self.y_pos - offset[1]))
+            else:
+                screen.blit(pygame.transform.flip(self.display_frame, True, False), (self.x_pos - offset[0], self.y_pos - offset[1]))
 
         for s in self.smokes[:]:
             if s.Expired:
                 self.smokes.remove(s)
             else:
                 s.display(screen, offset)
+                
+        if self.health<=0 and self.frame_index==5:
+            self.death_finished=True
 
     def update_animation(self, shot_bullets):
         current_time = pygame.time.get_ticks()
@@ -158,6 +161,8 @@ class Gunman:
                     self.frame_index += 1
                     self.last_animation_update = current_time
                 else:
+                    self.death_finished=True
+                    
                     self.status = 'death'
                     self.frame_index = len(self.death_frames) - 1
 
@@ -263,6 +268,7 @@ class Gunman:
             self.ALIVE = False
             self.status = "in the way to hell"
             self.frame_index = 0
+            
 
 
 # =============================================
