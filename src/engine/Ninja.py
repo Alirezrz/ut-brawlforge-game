d2ea0c22dcefaa86e6ee5cb38a3ede5f590c1a00
RmanDavoodi <armandavoodi12345@gmail.com>
2025-07-28 20:23:36 +0330

    ninja inputs

--- src/engine/Ninja.py
+++ src/engine/Ninja.py
@@ -821,11 +821,29 @@ class Ninja:
             self.stop_horizontal_movement()
                     # Add in handle_input
                     
-    def handle_input_online(self, keys, mouse_clicks):
-        if keys[pygame.K_d]:
-            print("moving right")
-        if keys[pygame.K_a]:
-            print("moving left")    
+    def handle_input_online(self, keys, gate, shot_bullets, bullet_class, trigger_shutter, mouse_buttons):
+            if keys[pygame.K_d]:
+                self.move_right()
+                self.is_moving_horizontally = True
+                print("Moving right...")
+            if keys[pygame.K_a]:
+                self.move_left()
+                self.is_moving_horizontally = True
+                print("Moving left...")
+            if keys[pygame.K_w]:
+                #self.jump()
+                print("jump...")
+            if keys[pygame.K_LSHIFT]:
+                self.activate_jetpack()
+            if keys[pygame.K_TAB]:
+                self.Send_teleport_request(gate)
+            if mouse_buttons[0]:
+                self.shoot(shot_bullets, bullet_class)
+
+            if keys[pygame.K_g]:
+                self.call_drone()
+            if mouse_buttons[2]:
+                self.shoot_rocket(shot_bullets, bullet_class)      
 
             
                 
