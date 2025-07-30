9050df10bff4dfdabefca38dbdc681cbcfa9b623
RmanDavoodi <armandavoodi12345@gmail.com>
2025-07-31 01:21:24 +0330

    fix lag

--- run_client.py
+++ run_client.py
@@ -63,7 +63,7 @@ class Client:
         self.screen_width=screen_width
         self.screen_height=screen_height
         self.load_assets()
-        self.opponent_character = None
+        self.opponent_character = ""
         self.opponent_frames = {"idle_frames": [pygame.Surface((50, 50))]}
         self.send_initial_data()
         self.x_pos=0
@@ -667,7 +667,6 @@ class Client:
                             self.frame_source = selfdata['frame_source']
                             self.frame_index = selfdata['frame_index']
                             self.character_name = selfdata.get("character", "Ninja")
-                            self.load_ui_assets(self.character_name)
                             for event in selfdata.get("events", []):
                                  self.play_sound(event, self.character_name)
 
