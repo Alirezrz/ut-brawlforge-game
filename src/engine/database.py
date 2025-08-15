65b512845cf78cd0cb32cfadc82e06b41b958114
RmanDavoodi <armandavoodi12345@gmail.com>
2025-08-15 19:00:33 +0330

    add db file

---
+++ src/engine/database.py
@@ -0,0 +1,12 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGODB_URI")) 
+db = client["my_game_db"]  
+users_collection = db["users"]  
+
+def get_user_info(username):
+    return users_collection.find_one({"username": username})
